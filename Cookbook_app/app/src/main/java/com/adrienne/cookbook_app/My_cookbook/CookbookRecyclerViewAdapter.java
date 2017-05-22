399768f060424e6809ee139247bbff1ba584c79c
Adrienne McCloud <mccloud.adrienne@gmail.com>
2017-05-21 20:15:14 -0400

    not finished error with fragments passing to detail view

---
+++ Cookbook_app/app/src/main/java/com/adrienne/cookbook_app/My_cookbook/CookbookRecyclerViewAdapter.java
@@ -0,0 +1,8 @@
+package com.adrienne.cookbook_app.My_cookbook;
+
+/**
+ * Created by Admin on 5/21/17.
+ */
+
+public class CookbookRecyclerViewAdapter {
+}
--- Cookbook_app/app/src/main/java/com/adrienne/cookbook_app/Search/ApiRecipeRecyclerViewAdapter.java
+++ Cookbook_app/app/src/main/java/com/adrienne/cookbook_app/Search/ApiRecipeRecyclerViewAdapter.java
@@ -1,9 +1,13 @@
 package com.adrienne.cookbook_app.Search;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.adrienne.cookbook_app.MainActivity;
 import com.adrienne.cookbook_app.R;
 import com.squareup.picasso.Picasso;
 
@@ -16,6 +20,7 @@ import java.util.List;
 public class ApiRecipeRecyclerViewAdapter extends RecyclerView.Adapter<ApiRecipeViewHolder>{
 
     private List<ApiRecipe> mApiRecipeList;
+    private SearchFragment.SearchOnFragmentInteractionListener  mSearchListner;
 
     public ApiRecipeRecyclerViewAdapter(List<ApiRecipe> apiRecipeList) {
         mApiRecipeList = apiRecipeList;
@@ -28,12 +33,32 @@ public class ApiRecipeRecyclerViewAdapter extends RecyclerView.Adapter<ApiRecipe
     }
 
     @Override
-    public void onBindViewHolder(ApiRecipeViewHolder holder, int position) {
+    public void onBindViewHolder(final ApiRecipeViewHolder holder, final int position) {
     final ApiRecipe currentApiRecipe = mApiRecipeList.get(position);
         Picasso.with(holder.mApiImage.getContext()).load(currentApiRecipe.getApiImage()).into(holder.mApiImage);
         holder.mApiTitle.setText(currentApiRecipe.getApiTitle());
         holder.mApiWebsiteSource.setText(currentApiRecipe.getApiWebsiteSource());
         holder.mApiServing.setText(String.valueOf(currentApiRecipe.getApiServings()));
+
+        holder.mRootview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ApiRecipe apiRecipe = mApiRecipeList.get(holder.getAdapterPosition());
+                String title = apiRecipe.getApiTitle();
+                String image =  apiRecipe.getApiImage();
+                String website = apiRecipe.getApiWebsiteSource();
+                String servings = apiRecipe.getApiServings().toString();
+                String url = apiRecipe.getApiUrl();
+                Intent displayApiRecipeIntent = new Intent(v.getContext().getApplicationContext(),
+                        MainActivity.class);
+                displayApiRecipeIntent.putExtra(MainActivity.KEY, title);
+                displayApiRecipeIntent.putExtra(MainActivity.KEY1, image);
+                displayApiRecipeIntent.putExtra(MainActivity.KEY2, website);
+                displayApiRecipeIntent.putExtra(MainActivity.KEY3, servings);
+                displayApiRecipeIntent.putExtra(MainActivity.KEY4, url);
+                v.getContext().startActivity(displayApiRecipeIntent);
+            }
+        });
 
     }
 
